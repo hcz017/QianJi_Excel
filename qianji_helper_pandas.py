fdd911f4f015e7b6f349b8a86fb21d2ab627f758
hcz017 <hcz017@gmail.com>
2023-07-04 01:37:20 +0800

    support ccbc bills(need to save as csv format first)

--- qianji_helper_pandas.py
+++ qianji_helper_pandas.py
@@ -13,7 +13,7 @@ USING_XLWINGS = True
 # 加载支付宝账单数据
 def load_alipay_bills(xlsx_path):
     print('load_alipay_bills')
-    # 跳过头部16行信息
+    # 跳过头部4行信息
     df = pd.read_csv(xlsx_path, encoding='gbk', skiprows=4)
     # print('df.columns\n', df.columns)
     # print('df content\n', df)
@@ -22,11 +22,11 @@ def load_alipay_bills(xlsx_path):
 
     # 1. 数据清洗（去除无效数据和不想要的数据）
     # 1.1 先删除含有 nan 的行
-    df1 = df.dropna(axis=0, how='any', thresh=None, subset=None, inplace=False)
+    df_lite = df.dropna(axis=0, how='any', thresh=None, subset=None, inplace=False)
     # 1.2 筛选出空白行并删除
     # 删除 订单编号 是空的，删除交易状态不是交易成功的
     to_drop_index = []
-    for index, row in df1.iterrows():
+    for index, row in df_lite.iterrows():
         if row['商家订单号               '].isspace():
             to_drop_index.append(True)
             continue
@@ -36,44 +36,44 @@ def load_alipay_bills(xlsx_path):
         to_drop_index.append(False)
     # 从dataframe 中删除过滤出的行
     empty_series = pd.Series(to_drop_index)
-    index_names = df1[empty_series].index
-    df1 = df1.drop(index_names, inplace=False)
+    index_names = df_lite[empty_series].index
+    df_lite = df_lite.drop(index_names, inplace=False)
 
     # 另一种遍历筛选方式：只筛选订单号为空的行
     # to_drop_index = []
-    # for order_id in df1['商家订单号               ']:
+    # for order_id in df_lite['商家订单号               ']:
     #     to_drop_index.append(order_id.isspace())
 
     # 2. 保留需要的列
     # # 方法一 按照列名称选择
     # select_cols = ['付款时间                ', '交易对方            ', '商品名称                ', '金额（元）   ',
     #                '收/支     ']
-    # df_filtered = df1[select_cols].copy()
-    # print('df1.columns', df1.columns)
+    # dst_df = df_lite[select_cols].copy()
+    # print('df_lite.columns', df_lite.columns)
     #
     # # 方法二 选择连续列
     # select_cols = df.columns[3: 7]
-    # df1 = df[select_cols]
-    # print('df1.columns', df1.columns)
+    # df_lite = df[select_cols]
+    # print('df_lite.columns', df_lite.columns)
 
     # 方法三 选择所有行和指定列
-    df_filtered = df1.iloc[:, [3, 7, 8, 9, 10]].copy()
+    dst_df = df_lite.iloc[:, [2, 7, 8, 9, 10]].copy()
     # print('df2.columns\n', df2.columns)
 
     # 3. 重命名列名称
-    df_filtered.columns = ['时间', '交易对方', '商品名称', '金额', '类型']
+    dst_df.columns = ['时间', '交易对方', '商品名称', '金额', '类型']
     # 4. 补充（新增）需要的列
     # 新增一列并赋值
-    df_filtered['账户1'] = '支付宝'
-    df_filtered['账户2'] = ''
-    df_filtered['分类'] = ' '
+    dst_df['账户1'] = '支付宝'
+    dst_df['账户2'] = ''
+    dst_df['分类'] = ' '
     # 新增一列并用另一列的值赋值
-    df_filtered['备注'] = df_filtered['交易对方']
-    df_filtered['账单图片'] = ''
+    dst_df['备注'] = dst_df['交易对方']
+    dst_df['账单图片'] = ''
     # 5. 排序
-    df_x = df_filtered[['时间', '分类', '类型', '金额', '账户1', '账户2', '备注', '账单图片', '交易对方', '商品名称']]
+    dst_df = dst_df[['时间', '分类', '类型', '金额', '账户1', '账户2', '备注', '账单图片', '交易对方', '商品名称']]
     print('load_alipay_bills done')
-    return df_x
+    return dst_df
 
 
 # 加载微信账单
@@ -82,20 +82,71 @@ def load_wechat_bills(xlsx_path):
     # 跳过头部16行信息
     df = pd.read_csv(xlsx_path, skiprows=16)
     # 删除不需要的列
-    df_filtered = df.drop(columns=['支付方式', '当前状态', '交易单号', '商户单号', '备注'], inplace=False)
+    df_lite = df.drop(columns=['支付方式', '当前状态', '交易单号', '商户单号', '备注'], inplace=False)
     # 列重命名
-    df_filtered.columns = ['时间', '分类', '交易对方', '商品名称', '类型', '金额']
+    df_lite.columns = ['时间', '分类', '交易对方', '商品名称', '类型', '金额']
+    # 去除 ￥ 符号
+    df_lite['金额'] = df_lite['金额'].apply(lambda x: x[1:]).astype('float')
     # 新增一列并赋值
-    df_filtered['账户1'] = '微信'
-    df_filtered['账户2'] = ''
+    df_lite['账户1'] = '微信'
+    df_lite['账户2'] = ''
+    # 覆盖 分类 内容
+    df_lite['分类'] = ' '
     # 新增一列并用另一列的值赋值
-    df_filtered['备注'] = df['交易对方']
-    df_filtered['账单图片'] = ''
+    df_lite['备注'] = df['交易对方']
+    df_lite['账单图片'] = ''
     # 排序 本质上应该是按列组合成新的 dataframe
-    df_x = df_filtered[
+    dst_df = df_lite[
         ['时间', '分类', '类型', '金额', '账户1', '账户2', '备注', '账单图片', '交易对方', '商品名称']]
     print('load_wechat_bills_pandas done')
-    return df_x
+    return dst_df
+
+
+def load_ccbc_bills(xlsx_path):
+    print('load_ccbc_bills')
+    # 跳过头部5行信息
+    df = pd.read_csv(xlsx_path, skiprows=5)
+    # 删掉最后一行无效值
+    df_lite = df.drop(df.index[[-1]], inplace=False)
+    # 列名
+    # '记账日          ', '交易日期          ', '交易时间                ',
+    #        '支出                ', '收入                ', '账户余额          ',
+    #        '币种          ', '摘要            ', '对方账号          ', '对方户名          ',
+    #        '交易地点                '],
+    # df_lite = df.drop(columns=['记账日          ', '账户余额          ', '币种          ', '对方账号          '],
+    #                       inplace=False)
+    df_lite['时间'] = ''
+    df_lite['类型'] = ''
+    df_lite['金额'] = ''
+
+    rows, cols = df_lite.shape
+    for i in range(0, rows):
+        # 重新组合时间字符串
+        data = str(df_lite['交易日期          '][i])
+        data = data[0:4] + '/' + data[4:6] + '/' + data[6:8]
+        df_lite['时间'][i] = data + ' ' + str(df_lite['交易时间                '][i])
+
+        df_lite['类型'][i] = '支出' if float(
+            df_lite['支出                '][i].replace(',', '')) > 0.00 else '收入'
+        df_lite['金额'][i] = df_lite['支出                '][i] if df_lite['类型'][i] == '支出' else \
+            df_lite['收入                '][i]
+
+    df_lite = df_lite.drop(columns=['交易日期          ', '交易时间                '], inplace=False)
+
+    # 新增一列并赋值
+    df_lite['账户1'] = '建设银行'
+    df_lite['账户2'] = ''
+    df_lite['分类'] = ' '
+    df_lite['备注'] = df['对方户名          ']
+    df_lite['账单图片'] = ''
+    # 排序 本质上应该是按列组合成新的 dataframe
+    dst_df = df_lite[
+        ['时间', '分类', '类型', '金额', '账户1', '账户2', '备注', '账单图片', '对方户名          ',
+         '交易地点                ']]
+    # 列重命名
+    dst_df.columns = ['时间', '分类', '类型', '金额', '账户1', '账户2', '备注', '账单图片', '交易对方', '商品名称']
+    print('load_ccbc_bills done')
+    return dst_df
 
 
 class QianJiHelper(object):
@@ -147,12 +198,26 @@ def getfiles():
     bill_file = []
     # 只返回扩展名是 csv 和 xlsx 的文件
     for filename in filenames:
-        if str(filename).endswith('csv'):
-            bill_file.append(filename)
-        if str(filename).endswith('xlsx'):
+        if str(filename).endswith('csv') or str(filename).endswith('xlsx') or str(filename).endswith('xls'):
             bill_file.append(filename)
     print('bill_file:', bill_file)
     return bill_file
+
+
+def get_bills():
+    wechat_bills_df = pd.DataFrame()
+    alipy_bills_df = pd.DataFrame()
+    ccbc_bills_df = pd.DataFrame()
+    for file in bill_files:
+        if file.startswith('微信'):
+            wechat_bills_df = load_wechat_bills(xlsx_path=file)
+        if file.startswith('alipay'):
+            alipy_bills_df = load_alipay_bills(xlsx_path=file)
+        if file.startswith('交易明细'):
+            ccbc_bills_df = load_ccbc_bills(xlsx_path=file)
+    # 组合df
+    df_all = pd.concat([wechat_bills_df, alipy_bills_df, ccbc_bills_df])
+    return df_all
 
 
 if __name__ == '__main__':
@@ -165,24 +230,17 @@ if __name__ == '__main__':
     # 创建qianji 账单模板 excel
     # qianji_helper = QianJiHelper(xlsx_name=output_name)
 
-    wechat_bills_df = pd.DataFrame()
-    alipy_bills_df = pd.DataFrame()
-    for file in bill_files:
-        if file.startswith('微信'):
-            wechat_bills_df = load_wechat_bills(xlsx_path=file)
-        if file.startswith('alipay'):
-            alipy_bills_df = load_alipay_bills(xlsx_path=file)
-    # 组合df
-    df_all = pd.concat([wechat_bills_df, alipy_bills_df])
-
+    df_all = get_bills()
     # todo: refine category
     for index, row in df_all.iterrows():
         remark = row['备注']
+        if pd.isnull(remark):
+            continue
         if '食堂' in remark or '亚惠' in remark or '河南大饼' in remark or '避风塘' in remark:
             row['分类'] = '三餐'
-        if '高德' in remark:
+        elif '高德' in remark or '滴滴' in remark:
             row['分类'] = '打车'
-        if '钱大妈' in remark:
+        elif '钱大妈' in remark or '满彭' in remark:
             row['分类'] = '食材'
 
     # 创建qianji 账单模板 excel
